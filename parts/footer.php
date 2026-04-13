dfd2d551eac39d164dc4ed2121b8fe6f96a677a8
Adrián Čiffáry <110901462+ItzRegen@users.noreply.github.com>
2026-04-13 18:17:08 +0200

    Dynamický Footer
    Dynamické volanie Footeru cez funkciu include zo súboru parts

---
+++ parts/footer.php
@@ -0,0 +1,32 @@
+<!-- Footer -->
+    <footer class="bg-warning bg-gradient text-white text-center py-4 mt-auto">
+      <div class="container d-none d-md-flex flex-row justify-content-between mx-auto">
+        <div class="col-md-3 mb-2">
+          <h5 class="text-dark fw-bold text-start">O mne</h5>
+          <p class="small text-dark text-start w-75">
+            Som začínajúci web developer, ktorý sa zameriava na tvorbu moderných webových stránok.
+          </p>
+        </div>
+
+        <nav class="d-flex flex-column">
+          <a href="index.php" class="footer-link mx-2">Domov</a>
+          <a href="about.php" class="footer-link mx-2">O mne</a>
+          <a href="portfolio.php" class="footer-link mx-2">Portfólio</a>
+          <a href="contact.php" class="footer-link mx-2">Kontakt</a>
+        </nav>
+
+        <div class="col-md-3">
+          <h5 class="text-dark fw-bold text-start">Kontakt</h5>
+          <div class="small text-dark text-start">
+            <p class="mb-1">
+              Mail: <a href="mailto:info@mojadomena.sk" class="text-white">adrian.ciffary@gmail.com</a>
+            </p>
+            <p class="mb-1">
+              Instagram: <a href="https://www.instagram.com/ciffary.a" target="_blank" class="text-white">@ciffary.a</a>
+            </p>
+            <p class="mb-1">
+              Tel. číslo: <a href="tel:+421948005764" class="text-white">+421 948 005 764</a>
+            </p>
+          </div>
+        </div>
+      </div>
